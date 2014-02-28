1bbda7e35d2fe5e8fdf609b257b8700d9e72dd2e
JessicaYeh <jyeh@smu.edu>
2014-02-28 17:03:23 -0600

    map

---
+++ js/findLocation.js
@@ -0,0 +1,59 @@
+var geocoder;
+var map;
+function initialize(locations) {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(32.84200, -96.782460);
+  var mapOptions = {
+    zoom: 12,
+    center: latlng
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  codeAddresses(locations);
+}
+
+function codeAddresses(locations) {
+  for (var i in locations) {
+    var name = locations[i].name;
+    var address = locations[i].address;
+    var city = locations[i].city;
+    var state = locations[i].state;
+    var zipcode = locations[i].zipcode.toString();
+    var combinedAddress = address + ", " + city + ", " + state;
+    var info = "<h3>" + name + "</h3>" + "<p>" + combinedAddress + ", " + zipcode + "</p>";
+    console.log(info);
+    var zip = {'postalCode': zipcode};
+    var options = { 'address': combinedAddress, componentRestrictions: zip };
+    geocoder.geocode( options, geocodeCallback(info) );
+  }
+}
+
+function geocodeCallback(info) {
+  return function (results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+        
+        google.maps.event.addListener(marker, 'click', function() {
+          var infowindow = new google.maps.InfoWindow({
+              content: info
+          });
+          infowindow.open(map,marker);
+        });
+      } else {
+        console.log('Geocode was not successful for the following reason: ' + status);
+      }
+  }
+}
+
+window.addEventListener('load', function() {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+      if(request.readyState === 4) {
+          initialize(JSON.parse(request.responseText)); 
+      }
+  }
+  request.open("GET", "js/taco_truck_locations.json", true);
+  request.send();
+});
