f88d918f65a5b38e2783f78b51a5f26a1ba62bcc
ngangloff <mitwannabe@verizon.net>
2014-03-02 13:44:17 -0600

    Update getToppings.php
    IGNORE THIS

--- phpdoc/getToppings.php
+++ phpdoc/getToppings.php
@@ -1,7 +1,6 @@
 <?php
 function get_ToppingInfo(toppingID,toppingName,heat,price,type)
 {
-
 $sql toppinginfo = "SELECT tacoTopping_id, topping_name, topping_heat, topping_price, topping_type from tacos natural join tacoToppings natural join toppings LIMIT 0, 30 ";
 echo toppinginfo;
 }
