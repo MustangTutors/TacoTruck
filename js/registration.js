b256dba965661dfdd0278e0b72995feb3327ad6a
JessicaYeh <jyeh@smu.edu>
2014-03-02 16:51:21 -0600

    working registration

--- js/registration.js
+++ js/registration.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
-	$("form input[type='submit']").click(function(event){
+	$(document).on('submit', 'form', function(event){
+        event.preventDefault();
+
 		var password = $("#registration_password").val();
 		var checkPass = $("#registration_confirm").val();
 
 		if(password !== checkPass) {
-			event.preventDefault();
 			$(".error").html("Error: Password confirmation did not match.<br/><br/>");
 		}
-		else{
+		else {
+            $.ajax({
+                type: "POST",
+                url: "phpdoc/addUser.php",
+                data: $(this).serialize(),
+                success: function(output) {
+                    if (output === "1") {
+                        window.location.replace("index.html");
+                    }
+                    else {
+                        $(".error").html("Error: That email already exists.<br/><br/>");
+                    }
+                }
+            });
 			$(".error").html("");
 		}
-	})
+	});
 });
