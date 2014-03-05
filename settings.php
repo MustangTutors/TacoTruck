71a9a0a962a35ecfae4ca8e9edbf53d1b47b1a6f
Jessica Yeh <jyeh@smu.edu>
2014-03-04 20:39:17 -0600

    fixed a syntax error

--- settings.php
+++ settings.php
@@ -18,6 +18,6 @@ define ("DB_NAME","TacoTruck");/*
      define ("DB_HOST","127.0.0.1");
      define ("DB_USER","root");
      define ("DB_PASSWORD","");
-     define ("DB_NAME","TacoTruck");*/
- }
+     define ("DB_NAME","TacoTruck");
+ }*/
 ?>
